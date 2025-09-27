677e7ebfffd34d7500275dd576a874d970478555
thisisminehoho <thisisminehoho@gmail.com>
2025-09-27 19:51:06 +0300

    Update urls.py

--- api_test/test1/urls.py
+++ api_test/test1/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path
-from test1.views import echo
+from . import views
 
 urlpatterns = [
+    path("echo/", views.echo, name="echo"),
 ]
+
