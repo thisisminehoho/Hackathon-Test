1b18b0d88c6ec2c7ec1907651492f97c53d4c523
thisisminehoho <thisisminehoho@gmail.com>
2025-09-27 19:52:39 +0300

    Update settings.py

--- api_test/api_test/settings.py
+++ api_test/api_test/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_test.test1',
+    'test1',
     'rest_framework',
 ]
 
@@ -122,4 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
